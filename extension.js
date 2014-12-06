f92c22e0f9ccf282e481d0e3ad258d1016dd03b5
Diskovr <pulsonic.kp@gmail.com>
2014-12-06 00:59:10 -0600

    scripts

---
+++ extension.js
@@ -0,0 +1,122 @@
+(function () {
+
+    //Define our function responsible for extending the bot.
+    function extend() {
+        //If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+            return setTimeout(extend, 1 * 1000);
+        }
+
+        //Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        //Load custom settings set below
+        bot.retrieveSettings();
+
+        /*
+         Extend the bot here, either by calling another function or here directly.
+         Model code for a bot command:
+
+         bot.commands.commandCommand = {
+         command: 'cmd',
+         rank: 'user/bouncer/mod/manager',
+         type: 'startsWith/exact',
+         functionality: function(chat, cmd){
+         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         else{
+         //Commands functionality goes here.
+         }
+         }
+         }
+
+         */
+
+        bot.commands.diskovrCommand = {
+            command: 'diskovr',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (vif so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("Visit @Diskovr's Website http://Diskovr.net");
+                }
+            }
+        };
+		
+		bot.commands.scCommand = {
+			command: 'sc',
+			rank: 'user'
+			type: 'exact',
+			functionality: function (chat,cmd) {
+				if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+				if (!bot.commands.executable(this.rank, chat)) return void (0);
+				else {
+					API.sendChat("Visit @Diskovr's Soundcloud https://soundcloud.com/diskovr");
+				}
+			}
+		};
+
+        //Load the chat package again to account for any changes
+        bot.loadChat();
+
+    }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    localStorage.setItem("basicBotsettings", JSON.stringify({
+        botName: "UpgradeBot",
+        language: "english",
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+        maximumAfk: 120,
+        afkRemoval: true,
+        maximumDc: 60,
+        bouncerPlus: true,
+        lockdownEnabled: false,
+        lockGuard: false,
+        maximumLocktime: 10,
+        cycleGuard: true,
+        maximumCycletime: 10,
+        timeGuard: true,
+        maximumSongLength: 10,
+        autodisable: true,
+        commandCooldown: 30,
+        usercommandsEnabled: true,
+        lockskipPosition: 3,
+        lockskipReasons: [
+            ["theme", "This song does not fit the room theme. "],
+            ["op", "This song is on the OP list. "],
+            ["history", "This song is in the history. "],
+            ["mix", "You played a mix, which is against the rules. "],
+            ["sound", "The song you played had bad sound quality or no sound. "],
+            ["nsfw", "The song you contained was NSFW (image or sound). "],
+            ["unavailable", "The song you played was not available for some users. "]
+        ],
+        afkpositionCheck: 15,
+        afkRankCheck: "ambassador",
+        motdEnabled: true,
+        motdInterval: 10,
+        motd: "Welcome To Upgrade!",
+        filterChat: true,
+        etaRestriction: false,
+        welcome: false,
+        opLink: null,
+        rulesLink: null,
+        themeLink: null,
+        fbLink: null,
+        youtubeLink: "http://goo.gl/JWZmEe",
+        website: null,
+        intervalMessages: [],
+        messageInterval: 5,
+        songstats: false,
+        commandLiteral: "!",
+        blacklists: {
+            BL: "https://rawgit.com/Darkwing1012/DarkwingBot/master/blacklists/BL.json",
+        }
+    }));
+
+    //Start the bot and extend it when it has loaded.
+    $.getScript('https://raw.githubusercontent.com/Darkwing1012/DarkwingBot/master/DarkwingBot.js', extend);
+
+}).call(this);
